6325e2d63dd691778c6e4051f938eab1034c3a4a
tamashny <tamashny@gmail.com>
2026-03-10 22:48:43 +0300

    Potential buggs fix

--- НИРС2/hCVT model/ControMap.m
+++ НИРС2/hCVT model/ControMap.m
@@ -2,38 +2,43 @@ clear
 clc
 close all
 
-%% загрузка Excel
+%% Загрузка данных
 M = readmatrix('BSFC.xlsx');
+rpm    = M(:,1);
+torque = M(:,2);
+bsfc   = M(:,3);
 
-bsfc   = M(:,1);
-rpm    = M(:,2);
-torque = M(:,3);
+%% Создаём сетку для интерполяции
+[n, T] = meshgrid(linspace(0,5500,200), linspace(0,150,200));
 
-%% создаём сетку
-[n,T] = meshgrid(linspace(0,5500,200), ...
-                 linspace(0,150,200));
+%% Интерполяция поверхности (натуральная соседняя)
+f = fit([rpm, torque], bsfc, 'poly35');
+BSFC = f(n, T);
 
-%% АППРОКСИМАЦИЯ ПОВЕРХНОСТИ (вместо scatteredInterpolant)
-
-f = fit([rpm, torque], bsfc, 'poly55');
-
-BSFC = f(n,T);
-
-%% 3D поверхность
+%% Контурная карта поверхности
 figure
-surf(n,T,BSFC,'EdgeColor','none')
+contourf(n, T, BSFC, 30, 'LineColor', 'none')  % фоновые контуры
+hold on
+contour(n, T, BSFC, 30, 'k', 'LineWidth', 0.8) % чёрные линии интерполяции
+
+%% Наложение исходных контуров из таблицы красными линиями
+uniqueLevels = unique(bsfc);
+for i = 1:length(uniqueLevels)
+    level = uniqueLevels(i);
+    idx = bsfc == level;
+    x = rpm(idx);
+    y = torque(idx);
+    
+    % сортируем по углу обхода для красивой линии
+    xc = mean(x); yc = mean(y);
+    angles = atan2(y - yc, x - xc);
+    [~, order] = sort(angles);
+    
+    plot(x(order), y(order), 'r-', 'LineWidth', 1)  % тонкие красные линии
+end
+
 xlabel('n (rpm)')
 ylabel('T (Nm)')
-zlabel('BSFC')
-title('BSFC surface')
+title('BSFC map with original contours')
 colorbar
-view(45,30)
-shading interp
-
-%% контурная карта
-figure
-contourf(n,T,BSFC,75)
-xlabel('n (rpm)')
-ylabel('T (Nm)')
-title('BSFC map')
-colorbar
+grid on
